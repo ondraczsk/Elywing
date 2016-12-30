70cb83dc42a27b600a2827eaa815469d4606a90f
Infernus <mr.snehjain@gmail.com>
2016-12-30 17:14:39 +0530

    k

--- src/pocketmine/command/defaults/EnchantCommand.php
+++ src/pocketmine/command/defaults/EnchantCommand.php
@@ -63,7 +63,7 @@ class EnchantCommand extends VanillaCommand{
 
 		$enchantment = Enchantment::getEnchantment($enchantId);
 		if($enchantment->getId() === Enchantment::TYPE_INVALID){
-			$enchantment = Enchantment::getEnchantmentByName($enchantId);
+			$enchantment = Enchantment::getEffectByName($enchantId);
 			if($enchantment->getId() === Enchantment::TYPE_INVALID){
 	    		$sender->sendMessage(new TranslationContainer("commands.enchant.notFound", [$enchantment->getId()]));
 				return true;
